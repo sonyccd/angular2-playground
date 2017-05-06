4106b510f888937947360f8ce9556de48cd6ba5d
Brad Bazemore <bwbazemore@uga.edu>
2017-05-06 14:31:30 -0400

    added components

---
+++ src/app/app.component.js
@@ -0,0 +1,15 @@
+(function (app) {
+  app.AppComponent =
+      ng.core.Component({
+        selector: 'my-app',
+        template: '<h1>Hello Angular</h1>'
+      })
+          .Class({
+            constructor: function () {
+            }
+          });
+})(window.app || (window.app = {}));
+
+app.AppComponent =
+    ng.core.Component({})
+        .Class({});
